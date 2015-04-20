1b3bac861d3249b21f03ea185b839c4511ca2c48
Max Malm <benjick@dumfan.net>
2015-04-20 22:03:55 +0200

    calculate order

--- client/controllers/dropZoneCard.js
+++ client/controllers/dropZoneCard.js
@@ -4,10 +4,11 @@ Template.dropZoneCard.events({
 
     FS.Utility.eachFile(event, function(file) {
       var id = Files.insert(file);
+      var order = Attributes.find({collection: collection}).count();
       Attributes.insert({
         collection: collection,
         fileId: id._id,
-        order: 99
+        order: order
       });
     });
   }
