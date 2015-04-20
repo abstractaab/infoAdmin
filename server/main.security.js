a65b09e26fbc08a807ba17f8054efe65e8c88ea9
Max Malm <benjick@dumfan.net>
2015-04-20 12:52:40 +0200

    only admin can create new users

--- server/main.security.js
+++ server/main.security.js
@@ -1,4 +1,14 @@
 Files.files.permit(['insert', 'update', 'remove']).ifLoggedIn().apply();
 Attributes.permit(['insert', 'update', 'remove']).ifLoggedIn().apply();
 Collections.permit(['insert', 'update', 'remove']).ifLoggedIn().apply();
-	
+
+Accounts.validateNewUser(function (user) {
+	// Only admin can add users
+	var loggedInUser = Meteor.user();
+
+	if (Roles.userIsInRole(loggedInUser, ['admin'])) {
+		return true;
+	}
+
+	throw new Meteor.Error(403, "Not authorized to create new users");
+});
