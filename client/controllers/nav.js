42322ad5aa4214dae353611f072caf6cfca07491
Max Malm <benjick@dumfan.net>
2015-04-22 15:41:17 +0200

    add log out button

--- client/controllers/nav.js
+++ client/controllers/nav.js
@@ -2,3 +2,9 @@ Template.nav.onRendered(function() {
 	$('.collapsible').collapsible();
 	$(".button-collapse").sideNav();
 })
+
+Template.nav.events({
+	'click .logout': function () {
+		Meteor.logout();
+	}
+});
